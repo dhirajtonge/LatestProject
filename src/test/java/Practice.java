c1749008276677949af0f0ab20d9b92f6b62b0c5
Dhiraj <bharatkumar.jogdand@forgeahead.io>
2017-05-05 15:47:02 +0530

    pushing the code

---
+++ src/test/java/Practice.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Sameer on 5/5/2017.
+ */
+public class Practice {
+   @Test
+    public void firstmethod() throws InterruptedException {
+       WebDriver driver =new ChromeDriver();
+       System.setProperty("webdriver.chrome.driver","C:\\Users\\Sameer\\Desktop\\Drivers\\chromedriver.exe");
+       driver.get("http://www.google.com");
+       Thread.sleep(5000);
+       driver.close();
+   }
+}
